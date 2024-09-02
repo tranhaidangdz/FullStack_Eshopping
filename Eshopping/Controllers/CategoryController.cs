08cc86992ef2fff8a1d09ad5e3ae0fa26f30e23b
tranhdaidangdz <trandang211@gmail.com>
2024-09-02 15:38:35 +0700

    hien thi theo category va theo brand

--- Eshopping/Controllers/CategoryController.cs
+++ Eshopping/Controllers/CategoryController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Eshopping.Models;
+using Eshopping.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Eshopping.Controllers
 {
     public class CategoryController:Controller
     {
-        public IActionResult Index()
+        public readonly DataContext _dataContext;
+        public CategoryController(DataContext context)
         {
-            return View();
+            _dataContext = context;
+        }
+        public async Task< IActionResult> Index(String Slug ="")
+        {
+            CategoryModel category =_dataContext.Categories.Where(c =>c.Slug==Slug).FirstOrDefault();
+            if (category == null)
+                return RedirectToAction("Index");
+            var productsByCategory = _dataContext.Products.Where(p => p.CategoryId == category.Id);
+            return View( await productsByCategory.OrderByDescending(p=>p.Id).ToListAsync());
         }
     }
 }
