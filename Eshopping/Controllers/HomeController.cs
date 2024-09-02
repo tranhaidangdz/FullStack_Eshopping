1be651815ec71cc68ae4f0c6d23d2442149c63e7
tranhdaidangdz <trandang211@gmail.com>
2024-09-02 15:06:10 +0700

    hien thi san pham tu database

--- Eshopping/Controllers/HomeController.cs
+++ Eshopping/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using Eshopping.Models;
+using Eshopping.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Eshopping.Controllers
 {
     public class HomeController : Controller
-    {
+        {
+        private readonly DataContext _dataContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,DataContext context)
         {
             _logger = logger;
+            _dataContext= context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products =_dataContext.Products.Include("Category").Include("Brand").ToList();
+            return View(products);
         }
 
         public IActionResult Privacy()
--- Eshopping/Repository/SeedData.cs
+++ Eshopping/Repository/SeedData.cs
@@ -15,8 +15,8 @@ namespace Eshopping.Repository
                 BrandModel apple = new  BrandModel { Name = "Apple", Slug = "apple", Description = "Apple is Large Brand in the world ", Status = 1 };
                 BrandModel samsung = new BrandModel { Name = "Samsung", Slug = "samsung", Description = "samsung  is Large Brand in the world ", Status = 1 };
         _context.Products.AddRange(
-                            new ProductModel { Name="Macbook",Slug="Macbook",Description="Macbook id the best",Image="1.jpg",Category=macbook,Brand=samsung,Price=1234},
-                            new ProductModel { Name="Pc",Slug="Pc",Description="Pc id the best",Image="1.jpg",Category=pc,Brand=samsung,Price=1234}
+                            new ProductModel { Name="Macbook",Slug="Macbook",Description="Macbook id the best",Image="product1.jpg",Category=macbook,Brand=samsung,Price=1234},
+                            new ProductModel { Name="Pc",Slug="Pc",Description="Pc id the best",Image="product2.jpg",Category=pc,Brand=samsung,Price=1234}
                         );
                 _context.SaveChanges();
             }
