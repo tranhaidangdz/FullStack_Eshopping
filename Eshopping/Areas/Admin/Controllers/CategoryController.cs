61706ca4c5fe71b5bee469afc41c75d133bc0b8b
tranhdaidangdz <trandang211@gmail.com>
2024-09-07 20:33:05 +0700

    them danh muc san pham: category product

--- Eshopping/Areas/Admin/Controllers/CategoryController.cs
+++ Eshopping/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
-﻿using Eshopping.Repository;
+﻿using Eshopping.Models;
+using Eshopping.Repository;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace Eshopping.Areas.Admin.Controllers
@@ -19,5 +22,51 @@ namespace Eshopping.Areas.Admin.Controllers
 			return View(await _dataContext.Categories.OrderByDescending(P => P.Id).ToListAsync());
 
 		}
-	}
+        public IActionResult Create()  //lấy ra ds danh mục và thương hiệu sp
+        {
+            return View();
+        }
+
+        //form create danh muc:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryModel category)  //lấy ds và thương hiệu của form (nhận từ ng dùng ) sau đó so sánh với các sp đã có trong csdl 
+        {
+
+            if (ModelState.IsValid)
+            {
+                //code them du lieu san pham:
+                //TempData["success"] = "Model ok hết rồi";
+                category.Slug = category.Name.Replace(" ", "-");
+                var slug = await _dataContext.Categories.FirstOrDefaultAsync(p => p.Slug == category.Slug);
+
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Danh mục này đã có trong Database");
+                    return View(category);
+                }
+
+                _dataContext.Add(category);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Thêm danh mục thành công";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["error"] = "Model có 1 vài thứ đang bị lỗi";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            return View(category);
+        }
+    }
 }
