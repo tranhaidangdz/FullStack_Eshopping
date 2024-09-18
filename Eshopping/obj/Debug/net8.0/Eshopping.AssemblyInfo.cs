8f4074c57f47ecd1d5e14a23173e21b74cd90781
tranhdaidangdz <trandang211@gmail.com>
2024-09-18 08:15:31 +0700

    sua lai role

--- Eshopping/obj/Debug/net8.0/Eshopping.AssemblyInfo.cs
+++ Eshopping/obj/Debug/net8.0/Eshopping.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eshopping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ab3f56e3ae79c03dd93cae804210fd1d96e7d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68dc4aee4d50cc37d64e1fd16cb0b05206551fb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eshopping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eshopping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
